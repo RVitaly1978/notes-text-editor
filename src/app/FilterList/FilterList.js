97849719941214280dd3fc6935520442701bddfb
Vital Raicheu <rvitaly1978@gmail.com>
2021-06-22 15:05:53 +0300

    feat: add FilterList component

---
+++ src/app/FilterList/FilterList.js
@@ -0,0 +1,22 @@
+import FilterRecord from '../FilterRecord/FilterRecord';
+import s from './FilterList.module.scss';
+
+const FilterList = ({ tags }) => {
+  let filterList = null;
+
+  if (tags.length !== 0) {
+    filterList = tags.map((id) => (
+      <li key={id} className={s.listItem}>
+        <FilterRecord id={id} />
+      </li>
+    ));
+  }
+
+  return filterList ? (
+    <ul className={s.container}>
+      {filterList}
+    </ul>
+  ) : null;
+};
+
+export default FilterList;
