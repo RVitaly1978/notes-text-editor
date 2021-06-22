3543e499dee11305d3dabf1b3100c51babeb4478
Vital Raicheu <rvitaly1978@gmail.com>
2021-06-22 15:08:29 +0300

    feat: add NoteDate component and formattingDate function

---
+++ src/app/NoteDate/NoteDate.js
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { selectNoteById } from '../../features/notes/notesSlice';
+import formattingDate from '../../helpers/formattingDate';
+import s from './NoteDate.module.scss';
+
+const NoteDate = ({ id }) => {
+  const { createdAt, editAt } = useSelector((s) => selectNoteById(s, id));
+
+  let dateInfo = `created: ${formattingDate(createdAt)}`;
+  if (createdAt !== editAt) {
+    dateInfo = `edited: ${formattingDate(editAt)}`;
+  }
+
+  return <p className={s.dateInfo}>{dateInfo}</p>;
+};
+
+export default NoteDate;
